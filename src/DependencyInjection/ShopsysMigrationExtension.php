0854f6dfb28de95c0aedf9a1ac2b6dfad98314ea
Tomáš Ludvik <tomas.ludvik@shopsys.com>
2024-11-12 16:32:32 +0100

    resolved missing typehints reported to be introduced in next package versions

--- src/DependencyInjection/ShopsysMigrationExtension.php
+++ src/DependencyInjection/ShopsysMigrationExtension.php
@@ -14,7 +14,7 @@ class ShopsysMigrationExtension extends Extension
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
